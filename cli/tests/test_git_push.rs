64c6aa722ab98c5f1940f8f7c0dd29f5c5f69efb
Baltasar Dinis <bsdinis@mit.edu>
2025-02-06 23:04:11 +0000

    cli/tests: port test_git_push to gitoxide

--- cli/tests/test_git_push.rs
+++ cli/tests/test_git_push.rs
@@ -16,6 +16,7 @@ use std::path::Path;
 use std::path::PathBuf;
 
 use test_case::test_case;
+use testutils::git;
 
 use crate::common::CommandOutput;
 use crate::common::TestEnvironment;
@@ -1800,7 +1801,7 @@ fn test_git_push_conflicting_bookmarks(subprocess: bool) {
     let git_repo = {
         let mut git_repo_path = workspace_root.clone();
         git_repo_path.extend([".jj", "repo", "store", "git"]);
-        git2::Repository::open(&git_repo_path).unwrap()
+        git::open(&git_repo_path)
     };
 
     // Forget remote ref, move local ref, then fetch to create conflict.
@@ -2106,9 +2107,9 @@ fn test_git_push_to_remote_named_git(subprocess: bool) {
     let git_repo = {
         let mut git_repo_path = workspace_root.clone();
         git_repo_path.extend([".jj", "repo", "store", "git"]);
-        git2::Repository::open(&git_repo_path).unwrap()
+        git::open(&git_repo_path)
     };
-    git_repo.remote_rename("origin", "git").unwrap();
+    git::rename_remote(&git_repo, "origin", "git");
 
     let output = test_env.run_jj_in(&workspace_root, ["git", "push", "--all", "--remote=git"]);
     insta::allow_duplicates! {
@@ -2134,9 +2135,9 @@ fn test_git_push_to_remote_with_slashes(subprocess: bool) {
     let git_repo = {
         let mut git_repo_path = workspace_root.clone();
         git_repo_path.extend([".jj", "repo", "store", "git"]);
-        git2::Repository::open(&git_repo_path).unwrap()
+        git::open(&git_repo_path)
     };
-    git_repo.remote_rename("origin", "slash/origin").unwrap();
+    git::rename_remote(&git_repo, "origin", "slash/origin");
 
     let output = test_env.run_jj_in(
         &workspace_root,
